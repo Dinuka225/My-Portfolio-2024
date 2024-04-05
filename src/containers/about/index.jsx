a18a631a1a6c2767037048018c936d117b9b8a73
Dinuka225 <dinukasaranga25@gmail.com>
2024-04-05 19:20:09 +0530

    4th commit

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -30,7 +30,7 @@ const personalDeatils = [
 ];
 
 const About = () => {
-  const jobSummery = `As a dedicated undergraduate at UCSC, I specialize in frontend development with a growing proficiency in backend technologies. With hands-on experience in HTML5, CSS3, JavaScript, ReactJS, Node.js, and Express.js, Im poised to create engaging digital experiences. Explore my portfolio to see my projects and witness my passion for pushing the boundaries of web development.`;
+  const jobSummery = `As a dedicated undergraduate at UCSC, I specialized in frontend development with a growing proficiency in backend technologies. With hands-on experience in HTML5, CSS3, JavaScript, ReactJS, Node.js, and Express.js,I poised to create engaging digital experiences. Explore my portfolio to see my projects and witness my passion for pushing the boundaries of web development.`;
   return (
     <section id="about" className="about">
       <PageHeaderContent
@@ -69,7 +69,7 @@ const About = () => {
               Personal Information
             </h3>
             <ul>
-              {personalDeatils.map((item , i) => (
+              {personalDeatils.map((item, i) => (
                 <li key={i}>
                   <span className="title">{item.label}</span>
                   <span className="value">{item.value}</span>
