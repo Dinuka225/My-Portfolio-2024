a114d6f9b7d80ad71f5882a51d4248fe1d79f231
Dinuka225 <dinukasaranga25@gmail.com>
2024-04-07 10:30:08 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import "./App.scss";
-import { useLocation } from "react-router-dom";
+
 import Navbar from "./components/navBar";
 import ParticlesComponent from "./components/utils/particles";
-import AnimatedRoutes from "./components/AnimatedRoutes/";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+import Resume from "./containers/resume";
+import Skills from "./containers/skills";
+import Portfolio from "./containers/portfolio";
+import Contact from "./containers/contact";
+
+
 function App() {
   const location = useLocation();
 
@@ -11,12 +19,18 @@ function App() {
   return (
     <div className="App">
       {renderParticlesJsInHomePage && <ParticlesComponent id="particles" />}
-      
-        <Navbar />
-        <div className="App_main-page-content">
-        <AnimatedRoutes/>
-        </div>
-      
+
+      <Navbar />
+      <div className="App_main-page-content">
+      <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/portfolio" element={<Portfolio />}/>
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        
+      </div>
     </div>
   );
 }
